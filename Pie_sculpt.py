2fde4199efb4c0ef46bcd0f98ed300b95bb2c8ad
Jose Ant <garciagaete@gmail.com>
2016-12-01 01:48:09 +0100

    cambios pie sculpt

--- Pie_sculpt.py
+++ Pie_sculpt.py
@@ -1,11 +1,11 @@
 bl_info = {
     "name": "Pinceles_Pie_Menus",
     "author": "Jose Ant. Garcia",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (2, 76, 0),
     "description": "Custom Pie Menus",
     "category": "3D View",}
-    
+
 import bpy
 from bpy.types import Menu
 
@@ -14,16 +14,16 @@ class Espejo(bpy.types.Operator):
     bl_idname = "symetri.retopo"
     bl_label = "Symmetry"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
 
     def execute(self, context):
         bpy.ops.sculpt.dynamic_topology_toggle()
         bpy.ops.sculpt.symmetrize()
         bpy.ops.sculpt.dynamic_topology_toggle()
- 
+
         return {'FINISHED'}
 
-    
+
 class PieSculptPie(Menu):
     bl_idname = "pie.pinceles"
     bl_label = "Brochas Sculpt"
@@ -31,14 +31,14 @@ class PieSculptPie(Menu):
     def draw(self, context):
         layout = self.layout
         pie = layout.menu_pie()
-        
+
         box = pie.split().column()
         row = box.row(align=True)
         box.scale_x=0.5
         box.operator("paint.brush_select", text='Fill/Deepen', icon='BRUSH_FILL').sculpt_tool= 'FILL'
         box.operator("paint.brush_select", text='Flatten', icon='BRUSH_FLATTEN').sculpt_tool= 'FLATTEN'
         box.operator("paint.brush_select", text='Pinch', icon='BRUSH_PINCH').sculpt_tool= 'PINCH'
-        box.operator("paint.brush_select", text='Scrape', icon='BRUSH_SCRAPE').sculpt_tool='SCRAPE'        
+        box.operator("paint.brush_select", text='Scrape', icon='BRUSH_SCRAPE').sculpt_tool='SCRAPE'
 
         box = pie.split().column()
         row = box.row(align=True)
@@ -47,7 +47,7 @@ class PieSculptPie(Menu):
         box.operator("paint.brush_select", text='Twist', icon='BRUSH_ROTATE').sculpt_tool= 'ROTATE'
         box.operator("paint.brush_select", text='Snakehook', icon='BRUSH_SNAKE_HOOK').sculpt_tool= 'SNAKE_HOOK'
         box.operator("paint.brush_select", text='Nudge', icon='BRUSH_NUDGE').sculpt_tool='NUDGE'
-   
+
         box = pie.split().column()
         row = box.row(align=True)
         box.scale_x=0.5
@@ -60,13 +60,13 @@ class PieSculptPie(Menu):
         box.operator("paint.brush_select", text='Blob', icon='BRUSH_BLOB').sculpt_tool= 'BLOB'
         box.operator("paint.brush_select", text='Layer', icon='BRUSH_LAYER').sculpt_tool= 'LAYER'
         box.operator("paint.brush_select", text='Draw', icon='BRUSH_SCULPT_DRAW').sculpt_tool= 'DRAW'
- 
+
         pie.operator("paint.brush_select", text='Inflate/Deflate', icon='BRUSH_INFLATE').sculpt_tool='INFLATE'
-        
+
         pie.operator("paint.brush_select", text='Clay Strips', icon='BRUSH_CLAY_STRIPS').sculpt_tool='CLAY_STRIPS'
-        
+
         pie.operator("paint.brush_select", text='Crease', icon='BRUSH_CREASE').sculpt_tool='CREASE'
-        
+
 
         pie.operator("paint.brush_select", text="Clay", icon='BRUSH_CLAY').sculpt_tool='CLAY'
 
@@ -79,10 +79,10 @@ class PieSculpttres(Menu):
     def draw(self, context):
         layout = self.layout
         pie = layout.menu_pie()
-        
+
         sculpt = context.tool_settings.sculpt
         box = pie.split().box().column()
-        
+
         row = box.row(align=True)
         row.label(text="Symmetry:")
         row.prop(sculpt, "use_symmetry_x", text="x", toggle=True)
@@ -90,16 +90,16 @@ class PieSculpttres(Menu):
         row.prop(sculpt, "use_symmetry_z", text="z", toggle=True)
         row = box.row(align=True)
         row.label(text="Lock:    ")
-     
+
         row.prop(sculpt, "lock_x", text="x", toggle=True)
         row.prop(sculpt, "lock_y", text="y", toggle=True)
         row.prop(sculpt, "lock_z", text="z", toggle=True)
 
-        
+
         box = pie.split().box().column()
         row = box.split(align=True)
- 
-        row.label(text="Curves:")      
+
+        row.label(text="Curves:")
         row = box.row(align=True)
         row = box.split(align=True)
         row.operator("brush.curve_preset", icon='SMOOTHCURVE', text="").shape = 'SMOOTH'
@@ -111,18 +111,18 @@ class PieSculpttres(Menu):
         #2 - BOTTOM
         box = pie.split().box().column()
         row = box.split(align=True)
- 
-        row.label(text="Sculpt Tools:")      
+
+        row.label(text="Sculpt Tools:")
         row = box.row(align=True)
         row.scale_x=0.9
         row.operator("boolean.mask_extract", text="Extract Mask")
         row = box.row(align=True)
         row.operator("boolean.mod_xmirror", text="X-mirror")
-        
-       
+
+
         sculpt = context.tool_settings.sculpt
         box = pie.split().box().column()
-        
+
         row = box.row(align=False)
         row.scale_x=0.5
         if context.sculpt_object.use_dynamic_topology_sculpting:
@@ -142,7 +142,7 @@ class PieSculpttres(Menu):
 
         row.prop(sculpt, "detail_type_method", text="")
 
-        
+
         brush = context.tool_settings.sculpt.brush
         capabilities = brush.sculpt_capabilities
         box = pie.split().box().column()
@@ -151,10 +151,10 @@ class PieSculpttres(Menu):
         row = box.row(align=True)
 
         row.prop(brush, "use_accumulate", text="Accumulate")
-        
+
         sculpt = context.tool_settings.sculpt
         box = pie.split().box().column()
-       
+
         row = box.row(align=True)
         row.operator("symetri.retopo", text="Symmetrize", icon='MOD_MIRROR')
         row = box.row(align=True)
@@ -164,25 +164,31 @@ class PieSculpttres(Menu):
         row = box.row(align=True)
         wm = context.window_manager
         row.prop(wm, "flood_meshsculpt", "Flood Value")
-                                              
+
+        box = pie.split().box().column()
+        row = box.row(align=True)
+        row.scale_x = 1.3
+        row.prop(brush, "use_original_normal", toggle=True, icon_only=True)
+        row.prop(brush, "sculpt_plane", text="")
+
 addon_keymaps = []
 
 def register():
     bpy.utils.register_module(__name__)
-    
-    
- 
-    
+
+
+
+
     wm = bpy.context.window_manager
-    
+
     if wm.keyconfigs.addon:
- 
-        
+
+
         km = wm.keyconfigs.addon.keymaps.new(name='Sculpt')
         kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS')
         kmi.properties.name = "pie.pinceles"
-        
-        
+
+
         km = wm.keyconfigs.addon.keymaps.new(name='Sculpt')
         kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS', alt=True)
         kmi.properties.name = "pie.opciones"
@@ -190,8 +196,8 @@ def register():
 
 def unregister():
     bpy.utils.unregister_module(__name__)
-    
-    
+
+
     wm = bpy.context.window_manager
 
     if wm.keyconfigs.addon:
@@ -201,11 +207,8 @@ def unregister():
 
             wm.keyconfigs.addon.keymaps.remove(km)
 
-    
+
     del addon_keymaps[:]
 
 if __name__ == "__main__":
     register()
-
-
-
